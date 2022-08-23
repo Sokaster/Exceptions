92f6872da96bc207189d13332eaa89eb8e60d4fc
Sokaster <danenkov.dmitriy@list.ru>
2022-08-23 20:36:29 +0300

    done

---
+++ MistakesCheck/Employee.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MistakesCheck
+{
+    internal class Employee
+    {
+        private int _age;
+        private string _name;
+        private double _salary;
+        public Employee(int age, string name, double salary)
+        {
+            Age = age;
+            Name = name;
+            Salary = salary;
+        }
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value < 18)
+                {
+                    throw new ArgumentException();
+                    //  throw new Exception("Лицам до 18 регистрация запрещена");
+                }
+                else
+                {
+                    _age = value;
+                }
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+
+                if (string.IsNullOrEmpty(value) || value.Length >= 100)
+                {
+                    //  throw new Exception("Длина имени меньше 2 символов");
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    _name = value;
+                    Console.WriteLine($" Введенное имя: {value}");
+                }
+            }
+        }
+        public double Salary
+        {
+            get { return _salary; }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentNullException();
+                    //throw new Exception("Зарплата должна быть равна больше 0");
+                }
+                else
+                {
+                    _salary = value;
+                }
+            }
+        }
+    }
+}
---
+++ MistakesCheck/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MistakesCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age (WARNING! Guys Younger than 18 y.o. have no roots for registration process)");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your name (less than 100 symbols)");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Your Salary");
+            double salary = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Employee employee = new Employee(age, name, salary);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.TargetSite);
+                Console.WriteLine($"Stack Trace:{ex.StackTrace}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.TargetSite);
+                Console.WriteLine($"Stack Trace:{ex.StackTrace}");
+            }
+            finally
+            {
+                Console.WriteLine("Finally Process");
+            }
+            Console.WriteLine("AfterFinallyBlock");
+            Console.ReadKey();
+        }
+    }
+}
